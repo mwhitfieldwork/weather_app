24cbbf72ef4aa5e6b29a8481a4803ec24a814450
mwhitfieldwork <mwhitfieldwork@gmail.com>
2024-12-30 23:37:05 -0600

    Add Tasks and Routes

--- weather_app/src/app/app.module.ts
+++ weather_app/src/app/app.module.ts
@@ -4,13 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { RouterModule, Routes } from '@angular/router';
+import { TasksComponent } from './tasks/tasks.component';
+
+const routes: Routes = [
+  { path: '', component: TasksComponent },
+];
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
